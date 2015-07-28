94afdb0628743bdc53db5ce242b8028b16fb944d
Alex Kaiser <atkaiser@berkeley.edu>
2015-07-28 08:05:24 -0700

    Refactoring commute_times.

--- commute_times.py
+++ commute_times.py
@@ -23,16 +23,14 @@ import re
 import sys
 from datetime import datetime
 
-DEBUG = False
+DEBUG = True
 
 # Configs: Change these to what you want to record
 origin = "37+May+Ct,+Hayward,+CA+94544"
 destination = "777+Mariners+Island+Blvd,+San+Mateo,+CA+94404"
 
-def main():
-    data_file = sys.argv[1]
-    # Get the response from google with transit times
-    if len(sys.argv) >= 3 and sys.argv[2] == "-switch":
+def get_time(switch):
+    if switch:
         url = "https://www.google.com/maps/dir/" + destination + "/" + origin + "/"
     else:
         url = "https://www.google.com/maps/dir/" + origin + "/" + destination + "/"
@@ -41,6 +39,8 @@ def main():
     # Find transit times from google
     times = re.findall("In current traffic: (\d+) min", html)
     if DEBUG:
+        print url
+        print html
         print times
     # Print error if there were no times
     if len(times) == 0:
@@ -51,6 +51,9 @@ def main():
         time_int = int(time)
         if time_int < min:
             shortest_time = time_int
+    return shortest_time
+
+def write_time_to_file(data_file, shortest_time):
     # Write to data file
     with open(data_file, "a+") as data:
         time = datetime.now().strftime("%Y-%m-%d %H:%M")
@@ -60,4 +63,7 @@ def main():
         data.write(",".join(data_list) + "\n")
     
 if __name__ == '__main__':
-    main()
+    data_file = sys.argv[1]
+    switch = len(sys.argv) >= 3 and sys.argv[2] == "-switch"
+    time = get_time(switch)
+    write_time_to_file(data_file, shortest_time)

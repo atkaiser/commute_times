dc3d91d1ec6425bcbdf6a29c4c237ffaedb311bc
Alex Kaiser <atkaiser@berkeley.edu>
2015-12-04 23:01:36 -0800

    Modifications to web.py

--- web.py
+++ web.py
@@ -10,18 +10,20 @@ import cherrypy
 
 from commute_times import get_time
 
-class StringGenerator(object):
+class DirTimeWeb(object):
     @cherrypy.expose
     def index(self):
         return "Hello world!"
 
     @cherrypy.expose
-    def generate(self, length=8):
-        return ''.join(random.sample(string.hexdigits, int(length)))
-    
+    def status(self):
+        return "Server is up"
+
     @cherrypy.expose
     def time(self, origin, destination):
         return str(get_time(origin.replace(' ', '+'), destination.replace(' ', '+')))
 
 if __name__ == '__main__':
-    cherrypy.quickstart(StringGenerator())
+    cherrypy.config.update( {'server.socket_host': '0.0.0.0'} )
+    cherrypy.config.update( {'server.socket_port': 10000} )
+    cherrypy.quickstart(DirTimeWeb())
